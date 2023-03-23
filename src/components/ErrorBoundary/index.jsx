a6b59ef363a55ca0a9cd2dcc13b262bdbf2985cf
Rivenrem <rivensenik@gmail.com>
2023-03-23 18:41:38 +0300

    feat: add error  boundary

---
+++ src/components/ErrorBoundary/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+class ErrorBoundary extends React.Component {
+  state = {
+    hasError: false,
+    error: { message: "", stack: "" },
+    info: { componentStack: "" },
+  };
+
+  static getDerivedStateFromError = (error) => {
+    return { hasError: true };
+  };
+
+  componentDidCatch = (error, info) => {
+    this.setState({ error, info });
+  };
+
+  render() {
+    const { hasError, error, info } = this.state;
+
+    const { children } = this.props;
+
+    return hasError ? console.log(error, info) : children;
+  }
+}
+
+export default ErrorBoundary;
--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import { Provider } from "react-redux";
 import store from "Store/index.js";
 
 import App from "Components/App/index.jsx";
+import ErrorBoundary from "Components/ErrorBoundary/index.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <App />
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
       </Router>
     </Provider>
   </React.StrictMode>
